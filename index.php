06495f750cc7a85ad7c85dfc814e20f43adcdfc6
Marioalf2002 <marioalf200216@gmail.com>
2019-09-13 23:26:01 +0200

    Primera Prueba

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div id="header">
+        <ul class="nav">
+            <li><a href="">inicio</a>
+                <ul>
+                    <li><a href="">submenu1</a></li>
+                    <li><a href="form.php">Formulario</a></li>
+                    <li><a href="visualizar.php">Ver Carpetas</a></li>
+                    <li><a href="">contactos</a></li>
+                </ul>
+            </li>
+            <li><a href="">servicios</a>
+                <ul>
+                    <li><a href="">submenu2</a></li>
+                    <li><a href="">servicios</a></li>
+                    <li><a href="">acerca de </a></li>
+                    <li><a href="">contactos</a></li>
+                </ul>
+            </li>
+            <li><a href="">acerca de </a>
+                <ul>
+                    <li><a href="">submenu3</a></li>
+                    <li><a href="">servicios</a></li>
+                    <li><a href="">acerca de </a></li>
+                    <li><a href="">contactos</a></li>
+                </ul>
+            </li>
+            <li><a href="">contactos</a>
+                <ul>
+                    <li><a href=""> submenu4</a></li>
+                    <li><a href="">servicios</a></li>
+                    <li><a href="">acerca de </a></li>
+                    <li><a href="">contactos</a></li>
+                </ul>
+            </li>
+        </ul>
+   </div>
+</body>
+</html>
---
+++ visualizar.php
@@ -0,0 +1,30 @@
+<?php
+    $listar = null;
+    $directorio = opendir("../curso/");
+    while ($elemento = readdir($directorio)) {
+        if ($elemento != '.' && $elemento != '..') {
+            if (is_dir("../curso/" . $elemento)) {
+                $listar .= "<li><a href='../curso/$elemento' target='_blanck'>$elemento/</li>";
+            } else {
+                $listar .= "<li><a href='../curso/$elemento' target='_blanck'>$elemento</li>";
+            }
+        }
+    }
+    echo "<h3>Listado de archivos y carpetas del directorio curso/</h3>";
+    echo "<p>$listar</p>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/jquery.dataTables.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php" class="btn btn-secondary">Regresar</a>
+</body>
+</html>
